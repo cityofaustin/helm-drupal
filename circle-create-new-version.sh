945ba343eceaa147f6f98f53bf7dd52f1b378bfd
aaron-wager <aaron.wagner@austintexas.gov>
2023-12-08 12:54:00 -0600

    change git fetch uri

--- circle-create-new-version.sh
+++ circle-create-new-version.sh
@@ -23,10 +23,10 @@ git checkout .
 git fetch origin gh-pages
 git checkout gh-pages
 
-helm repo index . --url https://favish.github.io/helm-drupal
+helm repo index . --url https://cityofaustin.github.io/helm-drupal
 
-git config --global user.name "favish-ci"
-git config --global user.email "dev@favish.com"
+git config --global user.name "aaron-wagner"
+git config --global user.email "aaron.wagner@austintexas.gov"
 
 git add .
 git commit -m "Create $CHARTNAME version $CIRCLE_TAG [ci skip]"
